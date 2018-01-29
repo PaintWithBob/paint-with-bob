63e5696dde234b98e6410ae17020567296457437
Aaron Turner <aaron@aaronthedev.com>
2018-01-29 15:31:33 -0800

    Made a branch for joining users

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+// Join User
+router.post('/join', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
 module.exports = router;
