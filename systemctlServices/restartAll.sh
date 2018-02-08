d5d692d1e5eb57002e840a384e290d0cfab53556
Aaron Turner <aaron@aaronthedev.com>
2018-02-07 22:21:21 -0800

    Started a github hook service for git hook deploy

--- systemctlServices/restartAll.sh
+++ systemctlServices/restartAll.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
 
+# redirect stdout/stderr to a file
+exec &> restartAll.log
+
+# Echo Some Spacing
+
+echo " "
+echo " "
+echo " "
+
+echo "Script starting up..."
+
+echo "Pulling latest master..."
+
+git pull origin master
+
+echo "Restarting Paint with bob services..."
+
 # Requires Aaron's dotfiles :p
-ssrestart paintWithBobFrontendNgServe.service
-ssrestart paintWithBobBackend.service
-ssrestart paintWithBobStream.service
+sudo systemctl restart paintWithBobFrontendNgServe.service
+sudo systemctl restart paintWithBobBackend.service
+sudo systemctl restart paintWithBobStream.service
+
+echo "Done!"
+
+echo " "
+echo " "
+echo " "
