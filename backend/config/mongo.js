d8babd7cf985641c93bd7802accee7abfd5e5596
Aaron Turner <aaron@aaronthedev.com>
2018-02-07 16:22:25 -0800

    Added more .env options for mongo

--- backend/config/mongo.js
+++ backend/config/mongo.js
@@ -1,8 +1,8 @@
 function Mongo() {
 	var mongoCredentials = {
-		uri: 'localhost',
-		port: '27017',
-		db: 'paintwithbob',
+		uri: process.env.MONGO_URI || 'localhost',
+		port: process.env.MONGO_PORT || '27017',
+		db: process.env.MONGO_DB_PATH || 'paintwithbob',
 		credentials: {
 			user: process.env.MONGO_USERNAME,
 			pass: process.env.MONGO_PASSWORD,
