217f51b9da3526f0c0f82ae2899a946f636fb3df
Joshua Warejko <joshuawarejko@gmail.com>
2018-02-14 10:17:06 -0800

    Small update to how server uptime is displayed (#32)

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -8,8 +8,8 @@ router.get('/', function(req, res, next) {
   let uptimeArray = process.uptime().toString().split('.');
   let milliseconds = uptimeArray[1];
   let seconds = uptimeArray[0]%60;
-  let minutes = Math.floor(uptimeArray[0]/60);
-  let hours = Math.floor(uptimeArray[0]/3600);
+  let minutes = Math.floor((uptimeArray[0]/60)%60);
+  let hours = Math.floor((uptimeArray[0]/3600)%24);
   let days = Math.floor(uptimeArray[0]/86400);
   res.render('index', {
     title: 'Express',
