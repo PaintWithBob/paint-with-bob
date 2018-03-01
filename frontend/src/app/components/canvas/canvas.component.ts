f0a858d78af914d16ed112f66d9e53725f1249eb
Aaron Turner <aaron@aaronthedev.com>
2018-02-28 16:23:37 -0800

    Got non-current user canvases

--- frontend/src/app/components/canvas/canvas.component.ts
+++ frontend/src/app/components/canvas/canvas.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, OnChanges, OnDestroy } from '@angular/core';
+import { Component, Input, OnInit, AfterViewInit, OnChanges, OnDestroy } from '@angular/core';
 
 const CANVAS_UPDATE_EVENT_ID = 'CANVAS_UPDATE';
 
@@ -7,61 +7,85 @@ const CANVAS_UPDATE_EVENT_ID = 'CANVAS_UPDATE';
   templateUrl: './canvas.component.html',
   styleUrls: ['./canvas.component.scss']
 })
-export class CanvasComponent implements OnInit, OnChanges, OnDestroy {
+export class CanvasComponent implements AfterViewInit, OnChanges, OnDestroy {
 
+  @Input() socket: any;
+  @Input() user: any;
+
+  canvasElementId: string;
   canvas: any;
   brushColor: any = '#222';
   tools: any[];
   activeTool: any;
-  @Input() socket: any;
   socketInitialized: boolean = false;
   lcDrawingChangeListener: any;
+  isReadOnly: boolean = false;
 
-  constructor() {}
+  constructor() {
+    this.canvasElementId = `literally-canvas-${Math.floor(Math.random() * 100000).toString(36)}`;
+    console.log(this.canvasElementId);
+  }
 
-  ngOnInit() {
-    const options = {
-      primaryColor: this.brushColor,
-      secondaryColor: '#444',
-      backgroundColor: 'transparent',
-      strokeWidths: [1, 2, 5, 10, 20, 30],
-      defaultStrokeWidth: 10,
-      watermarkImage: '',
-      imageSize: {
-        width: 500,
-        height: 500
-      },
-      keyboardShortcuts: false,
-    };
-    this.canvas = (<any>window).LC.init(document.querySelector('#my-canvas'), options);
+  ngAfterViewInit() {
+    // Check if we have a user
+   if(this.user) {
+    // If so, this is a canvas meant for simply listening and rendering events
+    this.isReadOnly = true;
+   }
 
-    // Array of tools, add here to add new tools
-    this.tools = [
-      {
-        name: 'pencil',
-        icon: 'pencil',
-        tool: new (<any>window).LC.tools.Pencil(this.canvas)
-      },
-      {
-        name: 'eraser',
-        icon: 'eraser',
-        tool: new (<any>window).LC.tools.Eraser(this.canvas)
-      }
-    ];
+   // Initialize our canvas
+   this.initializeLiterallyCanvas();
+  }
 
-    // Default tool is pencil
-    this.activateTool(this.tools[0]);
+  initializeLiterallyCanvas() {
 
-    this.lcDrawingChangeListener = this.canvas.on('drawingChange', () => {
-      if (this.socket) {
-        // Get our canvasSnapshot
-        const canvasSnapshot = JSON.stringify(this.canvas.getSnapshot());
+    // Wrap in a timeout to fire off change detection
+    setTimeout(() => {
+      const options = {
+        primaryColor: this.brushColor,
+        secondaryColor: '#444',
+        backgroundColor: 'transparent',
+        strokeWidths: [1, 2, 5, 10, 20, 30],
+        defaultStrokeWidth: 10,
+        watermarkImage: '',
+        imageSize: {
+          width: 500,
+          height: 500
+        },
+        keyboardShortcuts: false,
+      };
+      this.canvas = (<any>window).LC.init(document.querySelector(`#${this.canvasElementId}`), options);
 
-        // Emit to the server, which will then bounce to the approprite users
-        // TODO: Pass the actual user
-        this.socket.emit(CANVAS_UPDATE_EVENT_ID, {
-          user: CANVAS_UPDATE_EVENT_ID,
-          snapshot: canvasSnapshot
+      // Array of tools, add here to add new tools
+      this.tools = [
+        {
+          name: 'pencil',
+          icon: 'pencil',
+          tool: new (<any>window).LC.tools.Pencil(this.canvas)
+        },
+        {
+          name: 'eraser',
+          icon: 'eraser',
+          tool: new (<any>window).LC.tools.Eraser(this.canvas)
+        }
+      ];
+
+      // Default tool is pencil
+      this.activateTool(this.tools[0]);
+
+      if(!this.isReadOnly) {
+        this.lcDrawingChangeListener = this.canvas.on('drawingChange', () => {
+          if (this.socket) {
+            // Get our canvasSnapshot
+            const canvasSnapshot = JSON.stringify(this.canvas.getSnapshot());
+
+            // Emit to the server, which will then bounce to the approprite users
+            // TODO: Pass the actual user
+            this.socket.emit(CANVAS_UPDATE_EVENT_ID, {
+              user: CANVAS_UPDATE_EVENT_ID,
+              snapshot: canvasSnapshot
+            });
+          }
         });
       }
     });
@@ -74,7 +98,10 @@ export class CanvasComponent implements OnInit, OnChanges, OnDestroy {
 
       // Add the canvas update event
       this.socket.on(CANVAS_UPDATE_EVENT_ID, (data) => {
-        console.log(CANVAS_UPDATE_EVENT_ID, data);
+        // check if this canvas has a user associated
+        if(this.user) {
+          console.log('User canvas event!', this.user, data);
+        }
       });
     }
   }
