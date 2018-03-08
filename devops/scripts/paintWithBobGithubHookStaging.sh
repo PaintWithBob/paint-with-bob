57f0667ebb97e61c1c03be1473bb5ac6459ef168
Aaron Turner <aaron@aaronthedev.com>
2018-03-07 16:38:37 -0800

    Finished github hook enviornments

--- devops/scripts/paintWithBobGithubHookStaging.sh
+++ devops/scripts/paintWithBobGithubHookStaging.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-githubhook --callback=/gitmasterpr push:paint-with-bob:refs/heads/master ./restartStaging.sh
+# Source our env file
+source .env
+
+echo "Using the Github Hook Secret: $GITHUB_HOOK_STAGING"
+
+githubhook --callback=/gitmasterpr --secret=$GITHUB_HOOK_STAGING push:paint-with-bob:refs/heads/master ./restartStaging.sh
