dc1dbf224784d089a4161f625f70bf2b22246f5e
Aaron Turner <aaron@aaronthedev.com>
2018-03-12 16:41:59 -0700

    Fixed performance issues with the canvas by rendering an svg instead

--- frontend/src/app/components/canvas/canvas.component.ts
+++ frontend/src/app/components/canvas/canvas.component.ts
@@ -2,7 +2,6 @@ import { Component, Input, OnInit, OnChanges, OnDestroy } from '@angular/core';
 import { AuthService } from '../../providers';
 
 declare var require: any;
-const ric = require('request-idle-callback');
 
 const CANVAS_UPDATE_EVENT_ID = 'CANVAS_UPDATE';
 
@@ -20,8 +19,9 @@ export class CanvasComponent implements OnInit, OnChanges, OnDestroy {
     @Input() isReadOnly: boolean = false;
 
     authUser: any;
-    canvasElementId: string;
     canvas: any;
+    canvasElementId: string;
+    canvasElement: Element;
     brushColor: any = '#222';
     secondaryColor: any = '#444';
     backgroundColor: any = '#fff';
@@ -44,52 +44,53 @@ export class CanvasComponent implements OnInit, OnChanges, OnDestroy {
 
         // Wrap in a timeout to fire off change detection
         setTimeout(() => {
-            const options = {
-                primaryColor: this.brushColor,
-                secondaryColor: this.secondaryColor,
-                backgroundColor: this.backgroundColor,
-                strokeWidths: [1, 2, 5, 10, 20, 30],
-                defaultStrokeWidth: 10,
-                watermarkImage: '',
-                imageSize: {
-                    width: 500,
-                    height: 500
-                },
-                keyboardShortcuts: false
-            };
-            this.canvas = (<any>window).LC.init(document.querySelector(`#${this.canvasElementId}`), options);
 
-            // Array of tools, add here to add new tools
-            this.tools = [
-                { name: 'pencil', icon: 'pencil', tool: new (<any>window).LC.tools.Pencil(this.canvas) },
-                { name: 'eraser', icon: 'eraser', tool: new (<any>window).LC.tools.Eraser(this.canvas) },
-                { name: 'line', icon: 'minus', tool: new (<any>window).LC.tools.Line(this.canvas) },
-                { name: 'rectangle', icon: 'stop', tool: new (<any>window).LC.tools.Rectangle(this.canvas) },
-                { name: 'polygon', icon: 'play', tool: new (<any>window).LC.tools.Polygon(this.canvas) },
-                { name: 'ellipse', icon: 'circle', tool: new (<any>window).LC.tools.Ellipse(this.canvas) },
-                { name: 'text', icon: 'font', tool: new (<any>window).LC.tools.Text(this.canvas) },
-                { name: 'eyedropper', icon: 'magic', tool: new (<any>window).LC.tools.Eyedropper(this.canvas) }
-            ];
+            // Get our canvas element
+            this.canvasElement = document.querySelector(`#${this.canvasElementId}`);
 
-            // Default tool is pencil
-            this.activateTool(this.tools[0]);
-
+            // Read only canvases will simply embed svgs
             if(!this.isReadOnly) {
-                this.lcDrawingChangeListener = this.canvas.on('drawingChange', () => {
-                    if (this.socket) {
-                        // Get our canvasSnapshot
-                        const canvasSnapshot = JSON.stringify(this.canvas.getSnapshot());
+              const options = {
+                  primaryColor: this.brushColor,
+                  secondaryColor: this.secondaryColor,
+                  backgroundColor: this.backgroundColor,
+                  strokeWidths: [1, 2, 5, 10, 20, 30],
+                  defaultStrokeWidth: 10,
+                  watermarkImage: '',
+                  imageSize: {
+                      width: 500,
+                      height: 500
+                  },
+                  keyboardShortcuts: false
+              };
+              this.canvas = (<any>window).LC.init(this.canvasElement, options);
 
-                        // Emit to the server, which will then bounce to the approprite users
-                        // Emit to the server, which will then bounce to the approprite users
-                        if(this.user) {
-                            this.socket.emit(CANVAS_UPDATE_EVENT_ID, {
-                                user: this.user,
-                                snapshot: canvasSnapshot
-                            });
-                        }
-                    }
-                });
+              // Array of tools, add here to add new tools
+              this.tools = [
+                  { name: 'pencil', icon: 'pencil', tool: new (<any>window).LC.tools.Pencil(this.canvas) },
+                  { name: 'eraser', icon: 'eraser', tool: new (<any>window).LC.tools.Eraser(this.canvas) },
+                  { name: 'line', icon: 'minus', tool: new (<any>window).LC.tools.Line(this.canvas) },
+                  { name: 'rectangle', icon: 'stop', tool: new (<any>window).LC.tools.Rectangle(this.canvas) },
+                  { name: 'polygon', icon: 'play', tool: new (<any>window).LC.tools.Polygon(this.canvas) },
+                  { name: 'ellipse', icon: 'circle', tool: new (<any>window).LC.tools.Ellipse(this.canvas) },
+                  { name: 'text', icon: 'font', tool: new (<any>window).LC.tools.Text(this.canvas) },
+                  { name: 'eyedropper', icon: 'magic', tool: new (<any>window).LC.tools.Eyedropper(this.canvas) }
+              ];
+
+              // Default tool is pencil
+              this.activateTool(this.tools[0]);
+
+              this.lcDrawingChangeListener = this.canvas.on('drawingChange', () => {
+                  if (this.socket) {
+                      // Emit to the server, which will then bounce to the approprite users
+                      if(this.user) {
+                          this.socket.emit(CANVAS_UPDATE_EVENT_ID, {
+                              user: this.user,
+                              svg: this.canvas.getSVGString()
+                          });
+                      }
+                  }
+              });
             }
         });
     }
@@ -108,15 +109,14 @@ export class CanvasComponent implements OnInit, OnChanges, OnDestroy {
 
                         // Check if we had a pending request
                         if (this.canvasUpdateEvent) {
-                            ric.cancelIdleCallback(this.canvasUpdateEvent);
+                            cancelAnimationFrame(this.canvasUpdateEvent);
                         }
 
                         // Wrap in a requestIdleCallback
-                        this.canvasUpdateEvent = ric.requestIdleCallback(() => {
+                        this.canvasUpdateEvent = requestAnimationFrame(() => {
                             this.canvasUpdateEvent = false;
-                            // Get the snapshot, and set it to our canvas
-                            const snapshot = JSON.parse(data.snapshot);
-                            this.canvas.loadSnapshot(snapshot);
+                            // Set the inner HTML of our canvas target
+                            this.canvasElement.innerHTML = data.svg;
                         });
                     }
                 });
