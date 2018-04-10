310affd686213df67b4164463d16c4d8860d7211
Aaron Turner <aaron@aaronthedev.com>
2018-04-09 17:09:10 -0700

    Started video tests

--- videoservice/lib/index.js
+++ videoservice/lib/index.js
@@ -1,11 +1,12 @@
+// Public CLI like node file
+
 // Our dependencies
-const fs = require('fs');
-const path = require('path');
 const chalk = require('chalk');
 const gradient = require('gradient-string');
 const figlet = require('figlet');
-const ffmpeg = require('fluent-ffmpeg');
-const NodeMediaServer = require('node-media-server');
+
+// Require our server
+const paintWithBobVideoServer = require('./server.js');
 
 // Set up our minimist CLI
 const argv = require('minimist')(process.argv.slice(2), {
@@ -63,62 +64,5 @@ if(VIDEO_DIRECTORY.charAt(VIDEO_DIRECTORY.length - 1) !== "/") {
 const STREAM_NAME = argv.name || 'paintwithbob-1';
 const PORT = argv.port || '8000';
 
-// Get all of the mp4 files in the video directory
-// Get all test roms for the directory
-const videoFilePaths = fs.readdirSync(VIDEO_DIRECTORY);
-const videos = videoFilePaths.filter((file) => {
-    return path.extname(file).toLowerCase() === '.mp4';
-});
-
-// Define our event handlers for ffmpeg
-const ffmpegStartNewVideo = () => {
-  // ffmpeg -re -i INPUT_FILE_NAME -c copy -f flv rtmp://localhost/live/STREAM_NAME
-  const videoPath = `${VIDEO_DIRECTORY}${videos[Math.floor(Math.random() * videos.length)]}`;
-  console.log(`Playing the video at: ${videoPath}`);
-  const ffmpegCommand = ffmpeg(videoPath)
-    // .seekInput('27:20.000') Seek input for debugging
-    .inputOptions('-re')
-    .videoCodec('libx264')
-    .audioCodec('copy')
-    .format('flv')
-    // setup event handlers
-    .on('end', () => {
-      ffmpegOnEnd();
-    })
-    .on('error', (error) => {
-      ffmpegOnError(error)
-    })
-    .save(`rtmp://localhost/live/${STREAM_NAME}`);
-};
-
-const ffmpegOnEnd = () => {
-  console.log('[PAINT WITH BOB]: done processing input stream');
-  ffmpegStartNewVideo();
-};
-
-const ffmpegOnError = (error) => {
-  console.log('an error happened: ' + error.message);
-  process.exit(1);
-};
-
-// Timeout to ensure the server starts running before trying to stream to it
-setTimeout(() => {
-  ffmpegStartNewVideo();
-}, 1000);
-
-// Start our live stream serve
-const nodeMediaServerConfig = {
-  rtmp: {
-    port: 1935,
-    chunk_size: 60000,
-    gop_cache: true,
-    ping: 60,
-    ping_timeout: 30
-  },
-  http: {
-    port: PORT,
-    allow_origin: '*'
-  }
-};
-var nodeMediaServer = new NodeMediaServer(nodeMediaServerConfig);
-nodeMediaServer.run();
+paintWithBobVideoServer.initialize(VIDEO_DIRECTORY, STREAM_NAME, PORT);
+paintWithBobVideoServer.startStream();
