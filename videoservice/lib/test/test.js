d4dbcf37c2180c6a98ae5cfe2187f38dc2216385
Aaron Turner <aaron@aaronthedev.com>
2018-04-11 15:03:59 -0700

    Wrote tests for video server

---
+++ videoservice/lib/test/test.js
@@ -0,0 +1,27 @@
+// Assertion
+const assert = require('assert');
+
+// Require our server
+const paintWithBobVideoServer = require('../server.js');
+
+const SERVER_TIMEOUT = 3000;
+
+describe('Paint With Bob Video Service', () => {
+  it('Should be able to start and stop', function(done) {
+
+    // Timeout so the test doesn't fail too early
+    this.timeout(SERVER_TIMEOUT + 500);
+
+    // Initialize and start our stream
+    paintWithBobVideoServer.initialize('videoservice/droppy/files');
+    paintWithBobVideoServer.startStream();
+
+    // Timeout, close the server, and simply pass the test
+    // If no errors occured, we should be good! :)
+    setTimeout(() => {
+      paintWithBobVideoServer.stopStream();
+      assert.equal(true, true);
+      done();
+    }, SERVER_TIMEOUT);
+  });
+})
